2ca014625b3eb2b31d5162f49bdcc63c1d54e9d7
Iblis Lin <e196819@hotmail.com>
2015-07-25 23:52:59 +0800

    Introduce six

--- restkit/conn.py
+++ restkit/conn.py
@@ -5,12 +5,13 @@
 
 import logging
 import random
+import six
 import select
 import socket
 import ssl
 import time
-import cStringIO
 
+from six.moves import cStringIO as StringIO
 from socketpool import Connector
 from socketpool.util import is_connected
 
@@ -29,8 +30,8 @@ class Connection(Connector):
         self._s = backend_mod.Socket(socket.AF_INET, socket.SOCK_STREAM)
         self._s.connect((host, port))
         if proxy_pieces:
-            self._s.sendall(proxy_pieces)
-            response = cStringIO.StringIO()
+            self._s.sendall(six.b(proxy_pieces))
+            response = StringIO()
             while response.getvalue()[-4:] != '\r\n\r\n':
                 response.write(self._s.recv(1))
             response.close()
@@ -92,13 +93,13 @@ class Connection(Connector):
 
     def send_chunk(self, data):
         chunk = "".join(("%X\r\n" % len(data), data, "\r\n"))
-        self._s.sendall(chunk)
+        self._s.sendall(six.b(chunk))
 
     def send(self, data, chunked=False):
         if chunked:
             return self.send_chunk(data)
 
-        return self._s.sendall(data)
+        return self._s.sendall(six.b(data))
 
     def sendlines(self, lines, chunked=False):
         for line in list(lines):
